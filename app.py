ce646df54b7cb62340990740ec6837828cbeb2b2
André Luferat <contato@luferat.net>
2026-07-10 19:45:27 -0300

    mais coisas

--- app.py
+++ app.py
@@ -3,7 +3,12 @@
 name = "Joca da Silva"
 age = 35
 email = "joca@mail.com"
+birth = "10/04/1980"
+mother_fname = "Maria"
 
 print(name)
 print(age)
 print(email)
+print(birth)
+print(mother_fname)
+
