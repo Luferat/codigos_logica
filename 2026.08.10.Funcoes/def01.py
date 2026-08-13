5f96ae44f44719bd84de4a7101a97e4fe57269ea
André Luferat <contato@luferat.net>
2026-08-12 21:50:19 -0300

    ok

--- 2026.08.10.Funcoes/def01.py
+++ 2026.08.10.Funcoes/def01.py
@@ -1,12 +1,1 @@
-def my_function():
-  print("Hello from a function")
-  a = 20
-  if 10 == a:
-    print("É igual")
-  else:
-    print("É diferente")
-
-my_function()
-my_function()
-my_function()
-my_function()
+2026.08.10.Funcoes\def01.py
